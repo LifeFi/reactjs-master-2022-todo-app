9c0f2698d7cb6314488f97cca925ca41c1a93cd5
topcircle <topcircle@gmail.com>
2022-12-12 16:23:15 +0900

    6.18 enum

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,18 +1,27 @@
 import { atom, selector } from "recoil";
 
+// type categories = "TO_DO" | "DOING" | "DONE";
+
+export enum Categories {
+  "TO_DO" = "TO_DO",
+  "DOING" = "DOING",
+  "DONE" = "DONE",
+}
+
 export interface IToDo {
   text: string;
   id: number;
-  category: "TO_DO" | "DOING" | "DONE";
+  category: Categories;
 }
+
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
 });
 
-export const categoryState = atom({
+export const categoryState = atom<Categories>({
   key: "categoty",
-  default: "TO_DO",
+  default: Categories.TO_DO /**enum 사용시 실수를 줄일 수 있다. */,
 });
 
 export const toDoSelector = selector({
