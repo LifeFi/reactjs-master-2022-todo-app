ab1674e4680cb9d0e343b6dc56c234d1238fb695
topcircle <topcircle@gmail.com>
2022-12-12 11:54:02 +0900

    6.6

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,5 +1,49 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+/*
 function ToDoList() {
-  return null;
+  const [toDo, setToDo] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setToDo(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(toDo);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} placeholder="Write a to do" />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+}
+*/
+
+function ToDoList() {
+  const { register, watch } = useForm();
+  console.log(register("toDo"));
+  console.log(watch());
+
+  return (
+    <div>
+      <form>
+        <input {...register("email")} placeholder="Email" />
+        <input {...register("firstName")} placeholder="First Name" />
+        <input {...register("lastName")} placeholder="Last Name" />
+        <input {...register("username")} placeholder="Username" />
+        <input {...register("password")} placeholder="Password" />
+        <input {...register("password1")} placeholder="Password Confirmation" />
+
+        <button>Add</button>
+      </form>
+    </div>
+  );
 }
 
 export default ToDoList;
