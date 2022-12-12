d660ad9738e09cd44ea290e0a598c4122ccc2825
topcircle <topcircle@gmail.com>
2022-12-12 15:57:11 +0900

    6.17

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -5,20 +5,21 @@ export interface IToDo {
   id: number;
   category: "TO_DO" | "DOING" | "DONE";
 }
-
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
+});
+
+export const categoryState = atom({
+  key: "categoty",
+  default: "TO_DO",
 });
 
 export const toDoSelector = selector({
   key: "toDoSelector",
   get: ({ get }) => {
     const toDos = get(toDoState);
-    return [
-      toDos.filter((toDo) => toDo.category === "TO_DO"),
-      toDos.filter((toDo) => toDo.category === "DOING"),
-      toDos.filter((toDo) => toDo.category === "DONE"),
-    ];
+    const category = get(categoryState);
+    return toDos.filter((toDo) => toDo.category === category);
   },
 });
